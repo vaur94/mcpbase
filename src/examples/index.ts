5f701198181bf26181ecfe93c12d1569f3953b4b
vaur94 <umurat12@gmail.com>
2026-03-07 14:08:49 +0300

    build(package): restructure exports, peer deps, bump to v2.0.0

---
+++ src/examples/index.ts
@@ -0,0 +1,3 @@
+// Example tools will be moved here in Task 19
+// This file is a placeholder for the @vaur94/mcpbase/examples subpath export
+export {};
---
+++ src/security/index.ts
@@ -0,0 +1,3 @@
+// Security guards will be moved here in Task 19
+// This file is a placeholder for the @vaur94/mcpbase/security subpath export
+export {};
